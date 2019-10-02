0bfabf513ada1ba2a24076cfb70cd519a412dc3f
Matt Trovalli <mtrovalli@gmail.com>
2019-10-01 23:59:37 -0500

    finished data pre processing

--- data.py
+++ data.py
@@ -52,9 +52,135 @@ for key in key_ids:
         questions.append(mapping[key[i]])
         answers.append(mapping[key[i + 1]])
 
-# We now have two lists. questions[x] aligns with answers[x]
+"""
+We now have two lists. questions[x] aligns with answers[x].
+The next step is to clean the data, removing apostrophes, changing words and making everything lowercase.
+"""
 
-print(questions[123])
-print(answers[123])
+
+def clean(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"wouldn't", "would not", text)
+    text = re.sub(r"shouldn't", "should not", text)
+    text = re.sub(r"couldn't", "could not", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"isn't", "is not", text)
+    text = re.sub(r"didn't", "did not", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"  ", " ", text)
+    text = re.sub(r"[-()\"@#/:;<>{}+=~|.?,]", "", text)
+    return text
+
+# Clean the questions
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean(question))
+
+# Clean the answers
+clean_answers = []
+for answer in answers:
+    clean_answers.append(clean(answer))
+
+"""
+Next, we will remove the non-important words
+Note: I will come back and play with this. May be worth it to not do this.
+We do this to optimize training, but what if I had forever to train?
+
+Regardless, we will choose a dictionary to do this.
+"""
+
+word_count = {}
+
+for question in clean_questions:
+    for word in question.split(' '):
+        if word not in word_count:
+            word_count[word] = 1
+        else:
+            word_count[word] += 1
+
+for answer in clean_answers:
+    for word in answer.split(' '):
+        if word not in word_count:
+            word_count[word] = 1
+        else:
+            word_count[word] += 1
+
+"""
+Next:
+Tokenization and filtering the non frequent words. Both are key in NLP.
+Threshold is a hyperparameter
+"""
+
+threshold = 20
+questions_words_ints = {}
+word_number = 0
+for word, count in word_count.items():
+    if count >= threshold:
+        questions_words_ints[word] = word_number
+        word_number += 1
+
+answers_words_ints = {}
+word_number = 0
+for word, count in word_count.items():
+    if count >= threshold:
+        answers_words_ints[word] = word_number
+        word_number += 1
 
 
+# Adding the last tokens to these two dictionaries
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+for token in tokens:
+    questions_words_ints[token] = len(questions_words_ints) + 1
+for token in tokens:
+    answers_words_ints[token] = len(answers_words_ints) + 1
+
+# Create the inverse dictionary of answers_words_ints
+answersint2word = {w_i: w for w, w_i in answers_words_ints.items()}
+for i in range(len(clean_answers)):
+    clean_answers[i] += " <EOS>"
+
+# Translate all questions and answers into integers
+# and replace all words filtered out with <OUT>
+
+questions_to_int = []
+answers_to_int = []
+for question in clean_questions:
+    ints = []
+    for word in question.split():
+        if word not in questions_words_ints:
+            ints.append(questions_words_ints['<OUT>'])
+        else:
+            ints.append(questions_words_ints[word])
+    questions_to_int.append(ints)
+
+for answer in clean_answers:
+    ints = []
+    for word in answer.split():
+        if word not in answers_words_ints:
+            ints.append(answers_words_ints['<OUT>'])
+        else:
+            ints.append(answers_words_ints[word])
+    answers_to_int.append(ints)
+
+# Sort the questions and answers by length of question to help the training
+sorted_clean_questions = []
+sorted_clean_answers = []
+
+for length in range(1, 25 + 1):
+    for i in enumerate(questions_to_int):
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_to_int[i[0]])
+            sorted_clean_answers.append(answers_to_int[i[0]])
+
+### END DATA PRE PROCESSING ###
